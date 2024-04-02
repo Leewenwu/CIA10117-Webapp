019be76b4b1d53fefdcd773d0d5a10999ee6d39b
Bin <xd890872680@gmail.comgit config --listgit config --listfgit config --global user.email>
2024-04-02 10:00:48 +0800

    first commit

---
+++ src/main/java/com/tibame/CIA10117_Webapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tibame.CIA10117_Webapp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("你好HI");
+	} 
+
+}
